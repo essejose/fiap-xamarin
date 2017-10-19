0c2fe5f54de51b610103a794596446ea6b90b91b
essejose <ze_pk2@Hotmail.com>
2017-10-18 22:54:29 -0200

    exercice

---
+++ aula01/Exercice/XF.Enviar/XF.Enviar/XF.Enviar/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XF.Enviar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+             
+            MainPage = new NavigationPage(new XF.Enviar.MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ aula01/Exercice/XF.Enviar/XF.Enviar/XF.Enviar/ConfigPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Enviar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfigPage : ContentPage
+    {
+        public ConfigPage()
+        {
+            InitializeComponent();
+
+          
+        }
+                
+        private void swEmail_OnChanged(object sender, ToggledEventArgs e)
+        {
+            if (swEmail.IsEnabled) {
+                txtemail.IsEnabled = true;
+                
+            }
+            else
+            {
+                txtemail.IsEnabled = false;
+            }
+        }
+    }
+}

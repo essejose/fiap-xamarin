37bb78fc1c2fd25f3ab304a9439b93c34b2b2a43
unknown <rafael.calderani@gmail.com>
2017-11-01 22:50:43 -0200

    aula 03

---
+++ aula03/XF.Recursos/XF.Recursos/XF.Recursos/PassParameter/MCDetalheView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Recursos.Global;
+
+namespace XF.Recursos.PassParameter {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MCDetalheView : ContentPage {
+
+        public MCDetalheView() {
+           
+            MessagingCenter.Subscribe<MCHomeView, Contato>(this,
+                MessagingViewModel.Navegacao.Alterar.ToString(),
+               (navegarParametro, param) => {
+                   BindingContext = param;
+                   MessagingCenter.Unsubscribe<MCHomeView, Contato>(
+                       this, MessagingViewModel.Navegacao.Alterar.ToString());
+               });
+
+            InitializeComponent();
+
+
+        }
+
+    }
+}

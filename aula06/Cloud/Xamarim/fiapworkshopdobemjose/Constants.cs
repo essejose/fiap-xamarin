c9aa4ce2474d5064d92aa62a0b4b6c655455d86f
essejose <ze_pk2@hotmail.com>
2017-12-06 22:55:57 -0200

    aula nova

---
+++ aula06/Cloud/Xamarim/fiapworkshopdobemjose/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace fiapworkshopdobemjose
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://fiapworkshopdobemjose.azurewebsites.net";
+	}
+}
+
